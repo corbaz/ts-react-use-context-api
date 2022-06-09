e0b2a772380fde9ede5aa85b7c068a727b4cea73
Julio Corbaz <julio.corbaz@gmail.com>
2022-06-09 05:45:25 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// esta base se de usar para el deploy github pages
+// base: "/ts-react-use-context-api/",
+
 export default defineConfig({
-  //base: "/ts-react-use-context-api/",
   base: "/",
   build: {
     outDir: "dist",
